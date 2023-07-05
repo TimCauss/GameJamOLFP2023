f9d95507d1d3f8a8cc97d0b9ac2be494f30f6521
TimCauss <t.caussignac@gmail.com>
2023-07-05 11:30:36 +0200

    create Preloader scene

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Phaser from "../src/lib/phaser.js";
+import Preloader from "./scenes/Preloader.js";
 import Game from "./scenes/Game.js";
 import GameOver from "./scenes/GameOver.js";
 
 export default new Phaser.Game({
   type: Phaser.AUTO,
-  width: 480,
-  height: 640,
-  scene: [Game, GameOver],
+  width: 1920,
+  height: 1080,
+  scene: [Preloader, Game, GameOver],
   physics: {
     default: "arcade",
     arcade: {
---
+++ src/scenes/Preloader.js
@@ -0,0 +1,6 @@
+import Phaser from "../lib/phaser.js";
+
+export default class Preloader extends Phaser.Scene {
+    constructor() {
+        super("Preload");
+    }}
