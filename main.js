4bcc23eca8afa4dcd1c4ac8d7186ac1b58457ffc
TimCauss <t.caussignac@gmail.com>
2023-07-05 10:11:14 +0200

    feat: init Node, install Phaser, create the main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+import Phaser from "phaser";
+
+const config = {
+  type: Phaser.AUTO,
+  parent: "app",
+  pixelArt: true,
+  pysics: {
+    default: "arcade",
+    acracade: {
+      gravity: 0,
+      debug: true,
+    },
+  },
+  scene: [Preloader, StartMenu, Game, Interface, GameOver],
+  scale: {
+    autocenter: Phaser.scale.CENTER_BOTH,
+    mode: Phaser.Scale.FIT, // or FILL if you want to stretch the game
+    width: 960,
+    height: 640,
+  },
+};
