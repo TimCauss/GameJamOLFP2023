b058c6fc7e5bd3b72d1204be164d99278e9dec95
TimCauss <t.caussignac@gmail.com>
2023-07-05 11:54:15 +0200

    Co-authored-by: Najouay <Najouay@users.noreply.github.com> Co-authored-by: Jonathan <Dindon03@users.noreply.github.com> Co-authored-by: Binzen <BrunoZENARI@users.noreply.github.com>

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -3,4 +3,11 @@ import Phaser from "../lib/phaser.js";
 export default class Preloader extends Phaser.Scene {
     constructor() {
         super("Preload");
-    }}
+    }
+
+    Preload() {
+        this.load.image('Background1', "URL BACKGROUND1");
+    }
+    
+
+}
